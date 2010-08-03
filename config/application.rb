00902869d21c6e7a93469826e4bba849f54810a8
Fatdude <mark@fatdude.net>
2010-08-04 00:29:53 +0100

    Updated to rc

--- config/application.rb
+++ config/application.rb
@@ -11,17 +11,14 @@ module Timesheet3
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Add additional load paths for your own custom dirs
-    # config.load_paths += %W( #{config.root}/extras )
-    %w(observers mailers filters).each do |dir|
-      config.load_paths << "#{config.root}/app/#{dir}"
-    end
+    # Custom directories with classes and modules you want to be autoloadable.
+    config.autoload_paths += %W(#{config.root}/filters lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named
+    # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
-    # Activate observers that should always be running
+    # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
@@ -29,19 +26,18 @@ module Timesheet3
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Configure generators values. Many other options are available, be sure to check the documentation.
-    # config.generators do |g|
-    #   g.orm             :active_record
-    #   g.template_engine :erb
-    #   g.test_framework  :test_unit, :fixture => true
-    # end
+    # JavaScript files you want as :defaults (application.js is always included).
+    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+
+    # Configure the default encoding used in templates for Ruby 1.9.
+    config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters << :password
-    config.secret_token = '4ea05accbd2f9ed0f6f9f27c2076ae664e2cef5aa094d41288b5ac39d05fb49fff92d76c606bc0c0dc24d152034b9322bca8d7c51eba13116acd3fef30f74008'
+    config.filter_parameters += [:password]
+    config.secret_token = 'b75c78d01489abcdca32af616f26ef225b46a1acf589e34781793f6abc9ea61578eaf2e0d3871d4d7ee396b24c52b98dc43bb13f7416fbf1acedc808ed928822'
   end
 end
 
